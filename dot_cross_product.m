a70bf6f8cf049c4b798f0118cb15018cdb407f90
wong80 <zhiyuan3112@gmail.com>
2022-04-21 16:30:49 +0800

    Create dot_cross_product.m
    MATLAB Script to calculate  dot and cross product.

---
+++ dot_cross_product.m
@@ -0,0 +1,32 @@
+function []=dot_cross_product
+    [A,B]=fillarrays;
+    C=dotproduct(A,B);
+    D=crossproduct(A,B);
+    disp(C)
+    disp(D)
+end
+
+function [A,B]=fillarrays
+n=input("enter how many columns");
+A=zeros(1,n);
+B=zeros(1,n);
+    for i=1:1:n
+        a=input("input in values for first array in order: ");
+        A(1,i)=a;
+
+    end
+    for i=1:1:n
+        b=input("input in values for second array in order :");
+        B(1,i)=b;
+    end
+
+
+end
+
+function [C]=dotproduct(A,B)
+    C=dot(A,B);
+end
+
+function [D]=crossproduct(A,B)
+    D=cross(A,B);
+end
